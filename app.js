b32bc14f100ed9248f814c7914f62ae06603acf8
Luca <luca.confa80@gmail.com>
2017-10-12 07:26:37 +1100

    weather app with commnad

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('here we go');
+var weather = require('./weather.js');
+var location = require('./location.js');
+
+var argv = require('yargs')
+  .option('location',
+    {
+      demand: false,
+      alias: 'l',
+      describe: 'Enter Location',
+      type: 'string'
+    }).help('help')
+  .argv;
+
+if (typeof argv.l == 'string' && argv.l.length > 0) {
+
+  weather(argv.l, (currentWeather) => {
+    console.log(currentWeather);
+  });
+
+} else {
+
+  location((location) => {
+    if (location) {
+
+      weather(location.city, (currentWeather) => {
+        console.log(currentWeather);
+      });
+
+    } else {
+
+      console.log('Unable to guess location');
+
+    }
+  });
+
+}
